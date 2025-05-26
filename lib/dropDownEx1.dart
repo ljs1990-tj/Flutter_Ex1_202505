3689a5cd5807e963a786a739fe9c0034fe587f92
ljs1990-tj <ljs1990.tj@gmail.com>
2025-05-26 18:15:50 +0900

    2025.05.26 8교시

---
+++ lib/dropDownEx1.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DropDownEx1());
+}
+class DropDownEx1 extends StatefulWidget {
+  const DropDownEx1({super.key});
+
+  @override
+  State<DropDownEx1> createState() => _DropDownEx1State();
+}
+
+class _DropDownEx1State extends State<DropDownEx1> {
+  String selectItem = "Java";
+  List<String> list = ["Java", "Oracle", "Html/CSS", "Flutter"];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Scaffold(
+        body : Center(
+          child: Column(
+            children: [
+              DropdownButton(
+                  value : selectItem,
+                  icon: Icon(Icons.arrow_downward),
+                  style: TextStyle(color : Colors.blue),
+                  items: list.map((String item){
+                    return DropdownMenuItem(
+                        value : item,
+                        child: Text(item)
+                    );
+                  }).toList(),
+                  onChanged: (String? value){
+                    setState(() {
+                      selectItem = value!;
+                    });
+                  }
+              ),
+              Text("선택한 과목 : $selectItem"),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
