d18b3a03135991a8ea04c842f19231c7dcf09a05
ljs1990-tj <ljs1990.tj@gmail.com>
2025-05-30 18:22:50 +0900

    2025.05.30 8교시

--- lib/sampleUI/main.dart
+++ lib/sampleUI/main.dart
@@ -26,7 +26,7 @@ class _MainState extends State<Main> {
     return Scaffold(
       appBar: AppBar(title : Text("메인페이지")),
       drawer: Drawer(
-        child: ListView(
+        child: Column(
           children: [
             DrawerHeader(
                 decoration: BoxDecoration(
@@ -54,11 +54,70 @@ class _MainState extends State<Main> {
                 )
             ),
             Expanded(
-                child: ListTile(
-                  leading: Icon(Icons.home, color : Colors.indigo),
-                  title : Text("홈"),
-                  onTap: (){},
-                )
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.home, color : Colors.indigo),
+                      title : Text("홈"),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.shop, color : Colors.indigo),
+                      title : Text("쇼핑"),
+                      trailing: Text("new", style: TextStyle(color : Colors.redAccent),),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.category, color : Colors.indigo),
+                      title : Text("카테고리"),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.favorite, color : Colors.indigo),
+                      title : Text("내 위시리스트"),
+                      trailing: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color : Colors.redAccent
+                        ),
+                        child: Text("4", style : TextStyle(color : Colors.white)),
+                      ),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.shopping_cart, color : Colors.indigo),
+                      title : Text("장바구니"),
+                      trailing: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color : Colors.redAccent
+                        ),
+                        child: Text("2", style : TextStyle(color : Colors.white)),
+                      ),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.login, color : Colors.indigo),
+                      title : Text("로그인"),
+                      onTap: (){},
+                    ),
+                    Divider(height: 10,),
+                    ListTile(
+                      leading: Icon(Icons.settings, color : Colors.indigo),
+                      title : Text("설정"),
+                      onTap: (){},
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.logout, color : Colors.indigo),
+                      title : Text("로그아웃"),
+                      onTap: (){},
+                    ),
+                  ],
+                ),
+
             )
           ],
         ),
